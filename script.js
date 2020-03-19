0e28cfb8fa7b3e4cbd64cf052521a380c3171802
nirrek <kerrinenglish@gmail.com>
2020-03-19 11:36:11 +1100

    github yaml parser is wrong. yay

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-const { x, y, X, Y } = process.env;
+const { x, y } = process.env;
 
 // Do these types of logs in my script show up
 // by default in Github UI or will I also have to
 // do the weird secret setting shit to get it working
 console.log(JSON.stringify(process.env, null, 2));
-console.log('env variables', x, y, X, Y);
+console.log('env variables', x, y);
