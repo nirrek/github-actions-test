802be9cbed92b045e2e09f8a7b4edfe457fc108e
nirrek <kerrinenglish@gmail.com>
2020-03-20 16:05:04 +1100

    add new draftimage

--- 123-Worksheet.jsx
+++ 123-Worksheet.jsx
@@ -4,4 +4,9 @@
     url="https://i.imgur.com/lFN8s32.png"
     notesForImageCreator="Here is a note modified that I have changed"
   />
+  <DraftImage
+    id="9823944c-78f4-4fde-9118-f5b30d9bf1e1"
+    url="https://i.imgur.com/lFN8s32.png"
+    notesForImageCreator="Totally new draft image"
+  />
 </Worksheet>;
