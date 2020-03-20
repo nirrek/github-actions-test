23fb7241d397097c8ed88db4804810d2d0ce096a
nirrek <kerrinenglish@gmail.com>
2020-03-20 11:55:05 +1100

    Add draftimage

--- 123-Worksheet.jsx
+++ 123-Worksheet.jsx
@@ -1,3 +1,7 @@
 <Worksheet>
-  Hello
-</Worksheet>
+  <DraftImage
+    id="f275944c-78f4-4fde-9118-f5b30d9bf1e1"
+    url="https://i.imgur.com/lFN8s32.png"
+    notesForImageCreator="Here is a note"
+  />
+</Worksheet>;
